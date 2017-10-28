0ea14d283886588876b3f7136f68a22031f6c664
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2017-10-28 17:21:00 +0900

    - modify: the timing to stop timer

--- NZBase/Classes/NZNetworkIndicator.swift
+++ NZBase/Classes/NZNetworkIndicator.swift
@@ -11,15 +11,20 @@ open class NZNetworkIndicator: NSObject {
     public static let shared = NZNetworkIndicator()
     var indicatorStopTimer: Timer?
     
+    @objc public func forceStop() {
+        self.invalidateStopTimer()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
     public func start() {
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
         self.invalidateStopTimer()
-        self.indicatorStopTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.stop), userInfo: nil, repeats: false)
+        self.indicatorStopTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.forceStop), userInfo: nil, repeats: false)
     }
     
     @objc public func stop() {
         self.invalidateStopTimer()
-        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        self.indicatorStopTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.forceStop), userInfo: nil, repeats: false)
     }
     
     private func invalidateStopTimer() {
